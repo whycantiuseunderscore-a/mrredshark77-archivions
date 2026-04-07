5acd8f3a9d22dda7e457c5ae537c77a7b575f3e7
whycantiuseunderscore-a <arielcohen77@icloud.com>
2026-04-07 12:33:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function load2() {
-    document.getElementById("name_result").innerHTML = st_format(document.getElementById("number").value)
+    document.getElementById("name_result").innerHTML = square_format(document.getElementById("number").value) + "<br>" + st_format(document.getElementById("number").value)
 }
 
 let t = 0
